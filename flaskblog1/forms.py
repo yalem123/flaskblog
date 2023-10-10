098f8761887dea2bd317d98ab932d01c191f7456
yalem123 <yalembrhankelayneh@gmail.com>
2023-10-10 04:13:47 +0300

    fix

--- flaskblog1/forms.py
+++ flaskblog1/forms.py
@@ -14,13 +14,13 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
+    def validate_username(self, field):
+        user = User.query.filter_by(username=field.data).first()
         if user:
             raise ValidationError('That username is taken. Please choose a different one.')
 
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
+    def validate_email(self, field):
+        user = User.query.filter_by(email=field.data).first()
         if user:
             raise ValidationError('That email is taken. Please choose a different one.')
 
